39f24aee73e9d1ca03cc243ca6d03b2b219e3cc4
Han <gksdlrtn22@gmail.com>
2022-11-04 00:02:04 +0900

    22.11.04 파이썬 반복문 복습

--- Chapter05_예비/06. 반복문 개념_for.py
+++ Chapter05_예비/06. 반복문 개념_for.py
@@ -16,3 +16,15 @@ champions = ["티모", "이즈리얼", "리신"]
 
 for champion in champions:
     print("선택한 챔피언은", champion, "입니다.")
+
+# - 문자열 사용
+fighting_message = "자신감을 가지자! 나에게 한계란 없다!"
+
+for word in fighting_message:
+    print(word)
+
+# - range 객체 사용
+# range(10) -> 0~9까지 숫자를 포함하는 range 객체나온다. 0,1,2,3,4,5,6,7,8,9
+# range(시작, 끝+1)
+for i in range(1, 10, 2):
+    print(i)
